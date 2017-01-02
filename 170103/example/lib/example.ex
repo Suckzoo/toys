0ad95b3d1fb16388f77d3e21d88aac340cf48796
Seokju Hong <tjkj555@gmail.com>
2017-01-03 01:29:46 +0900

    Documentation practice

--- 170103/example/lib/example.ex
+++ 170103/example/lib/example.ex
@@ -1,2 +1,27 @@
 defmodule Example do
+  @moduledoc """
+  An example elixir app for newbie programmer
+  """
+  defmodule User do
+    @moduledoc """
+    Simple user struct
+    """
+    defstruct name: "Sean", job: "None"
+  end
+
+  @doc """
+  Simple greeting function.
+
+  ## Parameters
+  - name (string)
+
+  ## Examples
+  iex > Example.greet("suckzoo")
+
+  "Hi, suckzoo!"
+  """
+  @spec hello(String.t) :: (String.t)
+  def hello(name) do
+    "Hi, " <> name
+  end
 end
